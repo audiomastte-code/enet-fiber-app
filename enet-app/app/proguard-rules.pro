ab53402c9f0c5b98ff826d41fee7b0b2b0985aa6
audiomastte-code <audiomastte@gmail.com>
2026-09-02 23:36:24 -0500

    Delete enet-app directory

--- enet-app/app/proguard-rules.pro
+++
@@ -1,1 +0,0 @@
-# Reglas de ofuscación (vacío por ahora, WebView básico no requiere reglas especiales)
--- enet-app/app/src/main/java/pe/com/enet/app/EnetApp.kt
+++
@@ -1,5 +0,0 @@
-package pe.com.enet.app
-
-import android.app.Application
-
-class EnetApp : Application()
